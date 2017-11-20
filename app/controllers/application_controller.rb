05a7741326f3e0545be5cf6768791b61f9b3324f
Rushikesh Kawadkar <rash.kawadkar@hotmail.com>
2017-11-20 23:19:32 +0530

    k
    k

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def home
-    hashSequence = "mdxF6zzs|7121293|1|a|kamlesh|rash.kawadkar@hotmail.com| | | | | | | | | | |PAywewaLkM"
+    hashSequence = "ZiV9iaPC|7131293|1|a|kamlesh|rash.kawadkar@hotmail.com| | | | | | | | | | |7JBjEC9wox"
     @hash_value = Digest::SHA2.new(512).hexdigest(hashSequence)
     render template: "layouts/home"
   end
