6b3eb0a75c87d30f9794e0a61883e22a6261a813
Rushikesh Kawadkar <rash.kawadkar@hotmail.com>
2017-11-20 23:03:51 +0530

    test pay
    This commit refs- test me

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def home
     render template: "layouts/home"
+    hashSequence = "mdxF6zzs|7121293|1|a|kamlesh|rash.kawadkar@hotmail.com| | | | | | | | | | |PAywewaLkM"
+    @hash_value = Digest::SHA2.new(512).hexdigest(hashSequence)
   end
 end
