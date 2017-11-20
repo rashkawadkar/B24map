85179d450f9044213f3380b9d16d9ac9fa7a97c1
Rushikesh Kawadkar <rash.kawadkar@hotmail.com>
2017-11-20 23:15:19 +0530

    k
    test wrong

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def home
-    render template: "layouts/home"
     hashSequence = "mdxF6zzs|7121293|1|a|kamlesh|rash.kawadkar@hotmail.com| | | | | | | | | | |PAywewaLkM"
     @hash_value = Digest::SHA2.new(512).hexdigest(hashSequence)
+    render template: "layouts/home"
   end
 end
